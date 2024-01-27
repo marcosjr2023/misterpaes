37efcb411ac8234144823c22f09ae8342f89e950
marcosjr2023 <145724331+marcosjr2023@users.noreply.github.com>
2024-01-27 10:48:16 -0300

    subir arquivos

---
+++ enviar.php
@@ -0,0 +1,22 @@
+<?php
+
+    $nome = addcslashes($_POST['nome']);
+    $email = addcslashes($_POST['email']);
+    $celular = addcslashes($_POST['celular']);
+    $mensagem = addcslashes($_POST['mensagem']);
+
+    $destino = "marcosjr2520@gmail.com";
+    $assunto = "Coleta de Dados - Mister Pães";
+
+    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Celular: ".$celular."\n"."Mensagem: ".$mensagem;
+
+    $cabeca = "From eduardabais2005@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($destino,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }
+    else{
+        echo("Houve um erro ao enviar o E-mail!")
+    }
+
+?>
